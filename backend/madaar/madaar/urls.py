75c3142c99fbaf324e5fc3c7c011b6bdec489656
syrineaouf-create <syrine.aouf@polytechnicien.tn>
2025-12-11 19:46:35 +0100

    Mise à jour  backend

--- backend/madaar/madaar/urls.py
+++ backend/madaar/madaar/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     path('',home),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
+    path('api/', include('tickets.urls')),
+
 ]
